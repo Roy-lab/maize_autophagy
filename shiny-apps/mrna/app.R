cee73945b31251869106efc2d5e8149e31081d67
sgmccalla <grace.mccalla@gmail.com>
2025-11-19 19:56:28 -0600

    Updated index paths

--- shiny-apps/mrna/app.R
+++ shiny-apps/mrna/app.R
@@ -1,1 +1,18 @@
+library(shiny)
+# your other libraries
 
+## ---- UI ----
+ui <- fluidPage(
+  # ...
+)
+
+## ---- server ----
+server <- function(input, output, session) {
+  # ...
+}
+
+## ---- app object ----
+app <- shinyApp(ui = ui, server = server)
+
+## ---- return the app ----
+app
